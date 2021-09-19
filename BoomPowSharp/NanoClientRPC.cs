b4c5a0c7f5c73759612d4ff5adbb0f49f0939722
Adam Heaps <imod1998@outlook.com>
2021-09-19 18:17:39 +0100

    Cleanup/renaming.

--- BoomPowSharp/NanoClientRPC.cs
+++ BoomPowSharp/NanoClientRPC.cs
@@ -10,15 +10,13 @@ namespace BoomPowSharp
 {
     public class NanoClientRPC
     {
-        private SocketsHttpHandler _SocketHTTPHandler = new SocketsHttpHandler
+        private static SocketsHttpHandler _SocketHTTPHandler = new SocketsHttpHandler
         {
-            MaxConnectionsPerServer     = 10
+            MaxConnectionsPerServer = 10,
         };
 
-        private HttpClient _WebClient;
-        private Uri _WorkerUri;
-
-        static JsonSerializerOptions SerializeOptions = new JsonSerializerOptions { WriteIndented = false, IgnoreNullValues = true };
+        private static HttpClient            _WebClient       = new HttpClient(_SocketHTTPHandler);
+        private static JsonSerializerOptions SerializeOptions = new JsonSerializerOptions { WriteIndented = false, IgnoreNullValues = true };
 
         public class RPCRequest
         {
@@ -26,7 +24,7 @@ namespace BoomPowSharp
             public string Action { get; set; }
         }
 
-        public class WorkGenerateRequest : RPCRequest
+        public class RPCGenerateWorkRequest : RPCRequest
         {
             [JsonPropertyName("hash")]
             public string BlockHash { get; set; }
@@ -34,7 +32,7 @@ namespace BoomPowSharp
             public string Difficulty { get; set; }
         };
 
-        public class WorkGenerateResponse
+        public class RPCGenerateWorkResponse
         {
             [JsonPropertyName("error")]
             public string Error { get; set; }
@@ -46,13 +44,13 @@ namespace BoomPowSharp
             public string Multiplier { get; set; }
         };
 
-        public class WorkCancelRequest : RPCRequest
+        public class RPCCancelWorkRequest : RPCRequest
         {
             [JsonPropertyName("hash")]
             public string BlockHash { get; set; }
         };
 
-        public class WorkValidateRequest : RPCRequest
+        public class RPCValidateWorkRequest : RPCRequest
         {
             [JsonPropertyName("hash")]
             public string BlockHash { get; set; }
@@ -62,25 +60,100 @@ namespace BoomPowSharp
             public string Difficulty { get; set; }
         };
 
-        public class StatusResponse
+        public class RPCValidateWorkResponse
         {
-            [JsonPropertyName("generating")]
-            public string NumGenerating { get; set; }
-            [JsonPropertyName("queue_size")]
-            public string NumQueue { get; set; }
+            [JsonPropertyName("valid_all")]
+            public string _AllValid { get; set; }
+            [JsonPropertyName("valid_receive")]
+            public string _NumReceived { get; set; }
+            [JsonPropertyName("difficulty")]
+            public string Difficulty { get; set; }
+            [JsonPropertyName("multiplier")]
+            public string _Multiplier { get; set; }
+
+            //
+            // Conversions
+            //
+            [JsonIgnore]
+            public int AllValid { get { return int.Parse(_AllValid ?? "0"); } }
+            [JsonIgnore]
+            public int NumReceived { get { return int.Parse(_NumReceived ?? "0"); } }
+            [JsonIgnore]
+            public float Multiplier { get { return float.Parse(_Multiplier ?? "0"); } }
         };
 
-        public NanoClientRPC(Uri WorkerUri)
+        public class RPCBenchmarkRequest : RPCRequest
         {
-            _WorkerUri = WorkerUri;
-            _WebClient = new HttpClient(_SocketHTTPHandler);
+            [JsonPropertyName("count")]
+            public string Count { get; set; }
+        };
+
+        public class RPCBenchmarkResponse
+        {
+            [JsonPropertyName("average")]
+            public string _AverageTime { get; set; }
+            [JsonPropertyName("count")]
+            public string _Count { get; set; }
+            [JsonPropertyName("difficulty")]
+            public string Difficulty { get; set; }
+            [JsonPropertyName("duration")]
+            public string _Duration { get; set; }
+            [JsonPropertyName("hint")]
+            public string Hint { get; set; }
+            [JsonPropertyName("multiplier")]
+            public string _Multiplier { get; set; }
+
+            //
+            // Conversions
+            //
+            [JsonIgnore]
+            public int AverageTime { get { return int.Parse(_AverageTime ?? "0"); } }
+            [JsonIgnore]
+            public int Count { get { return int.Parse(_Count ?? "0"); } }
+            [JsonIgnore]
+            public int Duration { get { return int.Parse(_Duration ?? "0"); } }
+            [JsonIgnore]
+            public float Multiplier { get { return float.Parse(_Multiplier ?? "0"); } }
+        };
+
+        public class RPCStatusRequest : RPCRequest
+        {
+
+        }
+
+        public class RPCStatusResponse
+        {
+            [JsonPropertyName("generating")]
+            public string _NumGenerating { get; set; }
+            [JsonPropertyName("queue_size")]
+            public string _NumQueue { get; set; }
+
+            //
+            // Conversions
+            //
+            [JsonIgnore]
+            public int NumGenerating { get { return int.Parse(_NumGenerating ?? "0"); } }
+            [JsonIgnore]
+            public int NumQueue { get { return int.Parse(_NumQueue ?? "0"); } }
+        };
+
+        static NanoClientRPC()
+        {
             _WebClient.DefaultRequestHeaders.Clear();
             _WebClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        public async Task<WorkGenerateResponse> WorkGenerate(string BlockHash, string Difficulty)
+        private Uri _WorkerUri;
+        public Uri URL { get { return _WorkerUri; } }
+
+        public NanoClientRPC(Uri WorkerUri)
         {
-            var WorkGeneratePayload = new WorkGenerateRequest
+            _WorkerUri = WorkerUri;
+        }
+
+        public async Task<RPCGenerateWorkResponse> GenerateWork(string BlockHash, string Difficulty)
+        {
+            var WorkGeneratePayload = new RPCGenerateWorkRequest
             {
                 Action = "work_generate",
                 BlockHash = BlockHash,
@@ -89,40 +162,55 @@ namespace BoomPowSharp
 
             var HttpResult = await _WebClient.PostAsync(_WorkerUri, new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(WorkGeneratePayload, SerializeOptions))).ConfigureAwait(false);
 
-            return await JsonSerializer.DeserializeAsync<WorkGenerateResponse>(await HttpResult.Content.ReadAsStreamAsync());
+            return await JsonSerializer.DeserializeAsync<RPCGenerateWorkResponse>(await HttpResult.Content.ReadAsStreamAsync());
         }
 
-        public async Task WorkCancel( string BlockHash )
+        public Task<HttpResponseMessage> CancelWork( string BlockHash )
         {
-            var WorkGeneratePayload = new WorkCancelRequest {
+            var WorkGeneratePayload = new RPCCancelWorkRequest {
                 Action = "work_cancel",
                 BlockHash = BlockHash,
             };
 
-            await _WebClient.PostAsync(_WorkerUri, new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(WorkGeneratePayload, SerializeOptions))).ConfigureAwait(false);
+            return _WebClient.PostAsync(_WorkerUri, new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(WorkGeneratePayload, SerializeOptions)));
         }
 
-        public async Task<HttpResponseMessage> WorkValidate( string BlockHash, string WorkResult, string Difficulty)
+        public async Task<RPCValidateWorkResponse> ValidateWork( string BlockHash, string WorkResult, string Difficulty)
         {
-            var WorkGeneratePayload = new WorkValidateRequest {
+            var WorkGeneratePayload = new RPCValidateWorkRequest {
                 Action = "work_validate",
                 BlockHash = BlockHash,
                 WorkResult = WorkResult,
                 Difficulty = Difficulty
             };
 
-            return await _WebClient.PostAsync(_WorkerUri, new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(WorkGeneratePayload, SerializeOptions))).ConfigureAwait(false);
+            var Response = await _WebClient.PostAsync(_WorkerUri, new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(WorkGeneratePayload, SerializeOptions))).ConfigureAwait(false);
+
+            return await JsonSerializer.DeserializeAsync<RPCValidateWorkResponse>(await Response.Content.ReadAsStreamAsync(), SerializeOptions);
         }
 
-        public async Task<StatusResponse> Status( )
+        public async Task<RPCBenchmarkResponse> Benchmark(int Count)
         {
-            var WorkGeneratePayload = new RPCRequest {
+            var WorkGeneratePayload = new RPCBenchmarkRequest
+            {
+                Action = "status",
+                Count = Count.ToString()
+            };
+
+            var Response = await _WebClient.PostAsync(_WorkerUri, new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(WorkGeneratePayload, SerializeOptions))).ConfigureAwait(false);
+
+            return await JsonSerializer.DeserializeAsync<RPCBenchmarkResponse>(await Response.Content.ReadAsStreamAsync(), SerializeOptions);
+        }
+
+        public async Task<RPCStatusResponse> GetStatus( )
+        {
+            var WorkGeneratePayload = new RPCStatusRequest {
                 Action = "status"
             };
 
-            var HttpResult = await _WebClient.PostAsync(_WorkerUri, new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(WorkGeneratePayload, SerializeOptions))).ConfigureAwait(false);
+            var Response = await _WebClient.PostAsync(_WorkerUri, new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(WorkGeneratePayload, SerializeOptions))).ConfigureAwait(false);
 
-            return await JsonSerializer.DeserializeAsync<StatusResponse>(await HttpResult.Content.ReadAsStreamAsync());
+            return await JsonSerializer.DeserializeAsync<RPCStatusResponse>(await Response.Content.ReadAsStreamAsync(), SerializeOptions);
         }
     }
 }
